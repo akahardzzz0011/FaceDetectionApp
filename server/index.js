b00f876689369a9de0c2a896bd2d0e66b3e32424
Gerhard Klemola <gerhardcarlklemola@protonmail.com>
2023-03-24 21:08:48 +0000

    image read from filesystem

--- server/index.js
+++ server/index.js
@@ -1,17 +1,30 @@
 const express = require('express');
+const app = express();
 const cors = require('cors');
+const fs = require('fs').promises;
+
 const tf = require('@tensorflow/tfjs');
 const tfn = require('@tensorflow/tfjs-node-gpu');
-const handler = tfn.io.fileSystem('saved_model_tfjs/model.json')
-const app = express();
+const handler = tfn.io.fileSystem('saved_model_tfjs/model.json');
+
+const filePath = './uploads/'
+const testImage = 3
 app.use(cors());
 
+async function getFileFromPath(fileName) {
+  try {
+    const data = await fs.readFile(filePath + fileName);
+    console.log(data);
+  } catch (error) {
+    console.error(`Error occured when reading the file ${error.message}`)
+  }
+}
 
 async function loadModel() {
   const model = await tf.loadLayersModel(handler);
-  
- // let pred = model.predict(tf.tensor2d([[5, 3, 4, 6]], [1, 4]))
-  
+  //model.predict(tf.tensor4d([1], [1, 128, 128, 3])).print();
+  //model.predict(tf.tensor1d([testImage], [128, 128, 3])).print();
 }
 
 loadModel()
+getFileFromPath('test01.jpg')
